28dcd2159e656d1d6a563988ae3a1221ae9f0fc5
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-09-29 10:56:34 +0900

    바벨로 jsx를 구현해봄

--- hello-world/src/simple4.js
+++ hello-world/src/simple4.js
@@ -12,19 +12,17 @@ function LikeButton(){
 function Container(){
     const [count,setCount] = React.useState(0);
     return React.createElement(
-        'div',
-        null,
-        React.createElement(LikeButton),
-        React.createElement(
-            'div',
-            {style: {marginTop : 20}},
-        React.createElement('span',null,'현재 카운트 : '),
-        React.createElement('span',null,count),
-        React.createElement('button',{onClick:() => setCount(count+1)},'증가'),
-        React.createElement('button',{onClick:() => setCount(count-1)},'감소'),
-        ),
+        <div>
+            <LikeButton/>
+            <div style={{marginTop:20}}>
+                <span>현재 카운트</span>
+                <span>{count}</span>
+                <button onClick={()=>setCount(count+1)}>증가</button>
+                <button onClick={()=>setCount(count-1)}>감소</button>
+            </div>
+        </div>
     );
-};
+}
 const domContainer = document.querySelector("#react-root");
 
 ReactDOM.render(React.createElement(Container),domContainer);
